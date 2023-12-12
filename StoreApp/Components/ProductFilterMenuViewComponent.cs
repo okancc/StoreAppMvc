cb87813b19f8c4ed467f2d2655fc0eaa91df2e36
okancc <118931286+okancc@users.noreply.github.com>
2023-12-12 19:12:10 +0300

    Add FilteredByPrice

---
+++ StoreApp/Components/ProductFilterMenuViewComponent.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace StoreApp.Components
+{
+    public class ProductFilterMenuViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+        
+    }
+    
+}
