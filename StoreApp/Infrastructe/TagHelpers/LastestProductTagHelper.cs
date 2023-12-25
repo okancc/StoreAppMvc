8e96268ff5b58b79be1d00a66808918f9334bd87
okancc <118931286+okancc@users.noreply.github.com>
2023-12-25 23:57:14 +0300

    Add Page number

--- StoreApp/Infrastructe/TagHelpers/LastestProductTagHelper.cs
+++ StoreApp/Infrastructe/TagHelpers/LastestProductTagHelper.cs
@@ -9,6 +9,8 @@ namespace StoreApp.Infrastructe.TagHelpers
     public class LastestProductTagHelper : TagHelper
     {
         private readonly IServiceManager _manager;
+        [HtmlAttributeName("number")]
+        public int Number { get; set; }
 
         public LastestProductTagHelper(IServiceManager manager)
         {
@@ -32,7 +34,7 @@ namespace StoreApp.Infrastructe.TagHelpers
 
 
             TagBuilder ul = new TagBuilder("ul");
-            var products = _manager.ProductService.GetLastestProducts(5, false);
+            var products = _manager.ProductService.GetLastestProducts(Number, false);
             foreach (Product product in products)
             {
                 TagBuilder li = new TagBuilder("li");
